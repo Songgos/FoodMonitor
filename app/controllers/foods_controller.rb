f9e6232310974f5532a5eb029d0d23cd9f67f5e4
Kenny Valdivia <kenny.valdivia.cs1@gmail.com>
2015-12-01 19:16:29 -0500

    Some view changes for the CRUD

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -71,14 +71,6 @@ class FoodsController < ApplicationController
     # This method identifies the relationships between Food and ComponentType,
     # so those relationships can be rendered by the view.
     def set_properties
-      # Useful symbols to work with the queries in this method
-      :Vitamina
-      :Proteína
-      :Carbohidrato
-      :Mineral
-      :Grasa
-      @components_ids_marked = []
-
       @components_marked = Hash.new
       @components_all = Hash.new
 
@@ -93,17 +85,9 @@ class FoodsController < ApplicationController
         FoodsDatum.where(foods_id: params[:id]).each do |foods_datum|
           #TODO
           @components_marked[
-            Component.find(id: foods_datum.components_id).component_types_id
-            ] foods_datum.components_id
+            ComponentType.find(Component.find(foods_datum.components_id).component_types_id).name
+            ]= Component.find(foods_datum.components_id)
         end
-        @components_ids_marked.uniq
-
-        # Fetching the components names that match any value within the 
-        # @component_types_ids.
-        Component.where(id: @components_ids_marked).each do |component|
-          @components_marked.append component
-        end
-        @components_marked.uniq
       end
 
       # Deleting repeated values from all arrays
