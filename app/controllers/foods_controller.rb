24f7c591745c8cce2bcce420bd27c75374c19047
Kenny Valdivia <kenny.valdivia.cs1@gmail.com>
2015-12-03 19:34:13 +0000

    Changed some CRUD code, the interfaces needs some work

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,5 +1,6 @@
 class FoodsController < ApplicationController
   before_action :set_properties
+  before_action :product_components, only: [:set_properties]
   before_action :set_food, only: [:show, :edit, :update, :destroy]
 
   # GET /foods
@@ -26,10 +27,11 @@ class FoodsController < ApplicationController
   # POST /foods.json
   def create
     @food = Food.new(food_params)
-
     respond_to do |format|
       if @food.save
-        format.html { redirect_to @food, notice: 'Food was successfully created.' }
+        format.html { redirect_to @food,
+          notice: 'Food was successfully created.'
+        }
         format.json { render :show, status: :created, location: @food }
       else
         format.html { render :new }
@@ -63,42 +65,42 @@ class FoodsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_food
-      @food = Food.find(params[:id])
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_food
+    @food = Food.find(params[:id])
+  end
+
+  # This method identifies the relationships between Food and ComponentType,
+  # so those relationships can be rendered by the view.
+  def set_properties
+    @components_all = {}
+    # Store all components within a hass named after the ComponentType.name
+    ComponentType.all.each do |ct|
+      @components_all[ct.name] = Component.where(component_types_id: ct.id)
     end
 
-    # This method identifies the relationships between Food and ComponentType,
-    # so those relationships can be rendered by the view.
-    def set_properties
-      @components_marked = Hash.new
-      @components_all = Hash.new
+    product_components if params[:id]
+  end
 
-      # Store all components within a has named after the ComponentType.name
-      ComponentType.all.each do |component_type|
-        @components_all[component_type.name] = Component.where(component_types_id: component_type.id)
-      end
-
-      if params[:id]
-        # Fetching the component_id values for every Food matching the :id given
-        # within the "params" hash.
-        FoodsDatum.where(foods_id: params[:id]).each do |foods_datum|
-          #TODO
-          @components_marked[
-            ComponentType.find(Component.find(foods_datum.components_id).component_types_id).name
-            ]= Component.find(foods_datum.components_id)
-        end
-      end
-
-      # Deleting repeated values from all arrays
-      @component_names_marked = []
-      @component_ids_marked = []
-      @component_names_all = []
-      @component_ids_all = []      
+  def product_components
+    @product_components = {}
+    @components_all.keys.each do |ctn|
+      @product_components[ctn] = []
     end
-
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def food_params
-      params.require(:food).permit(:name, :description, :properties, :vitamins, :carbs, :fats, :minerals, :proteins)
+    FoodsDatum.where(foods_id: params[:id]).each do |fd|
+      @product_components[
+        ComponentType.find(
+          Component.find(fd.components_id)
+          .component_types_id).name].append Component.find(fd.components_id)
     end
+  end
+
+  # Never trust parameters from the scary internet,
+  # only allow the white list through.
+  def food_params
+    params.require(:food).permit(
+      :name, :description, :properties,
+      :vitamins, :carbs, :fats, :minerals, :proteins)
+  end
 end
